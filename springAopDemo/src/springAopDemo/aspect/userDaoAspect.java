5fe1bb90fd6eca3f45d84af7d3fdf2a521f3b19f
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-04-28 11:46:16 +0600

    pointcut declaration

--- springAopDemo/src/springAopDemo/aspect/userDaoAspect.java
+++ springAopDemo/src/springAopDemo/aspect/userDaoAspect.java
@@ -2,13 +2,17 @@ package springAopDemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class userDaoAspect {
 	
-	@Before("execution(public void save*() )")
+	@Pointcut("execution(public void save*() )")
+	void pointCut() {}
+	
+	@Before("pointCut()")
 	public void beforeSaveMethod() {
 		System.out.println("Before save method from aop");
 	}

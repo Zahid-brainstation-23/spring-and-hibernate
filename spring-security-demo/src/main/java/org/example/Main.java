7bbeab46a15fb98f0edaf2e2f34c48c5b82c6b18
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-05-10 19:53:23 +0600

    test to run spring security in spring
    need to fix some error

---
+++ spring-security-demo/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="org.example")
+@Controller  
+public class Main {
+	@Bean
+	public ViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+ 
+}

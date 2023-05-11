194c3e8a6d9213e7e5ebdc48183b303c9001b351
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-05-11 18:31:17 +0600

    simple spring boot app created for spring data rest

---
+++ data.rest.demo/src/main/java/com/spring/data/rest/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.spring.data.rest.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "Hellow world";
+    }
+}

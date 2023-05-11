04c249609518be4b448ece18a6161e1d05efa51d
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-05-11 20:19:52 +0600

    rest controller implemented

--- data.rest.demo/src/main/java/com/spring/data/rest/demo/controller/HomeController.java
+++ data.rest.demo/src/main/java/com/spring/data/rest/demo/controller/HomeController.java
@@ -1,13 +1,32 @@
 package com.spring.data.rest.demo.controller;
 
+import com.spring.data.rest.demo.entity.Customer;
+import com.spring.data.rest.demo.service.CustomerService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    private CustomerService customerService;
+
+    public HomeController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
     @GetMapping("/")
     public String homePage(){
         return "Hellow world";
+    }
+
+    @PostMapping ("/customer")
+    public Customer saveCustomer(@RequestBody Customer customer){
+        return customerService.saveCustomer(customer);
+    }
+
+    @GetMapping("/customer")
+    public String test(){
+        return "hehe";
     }
 }
--- data.rest.demo/src/main/java/com/spring/data/rest/demo/service/CustomerService.java
+++ data.rest.demo/src/main/java/com/spring/data/rest/demo/service/CustomerService.java
@@ -1,15 +1,21 @@
 package com.spring.data.rest.demo.service;
 
+import com.spring.data.rest.demo.entity.Customer;
 import com.spring.data.rest.demo.jpa.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomerService {
-
 
     private CustomerRepository customerRepository;
 
     CustomerService(CustomerRepository customerRepository){
         this.customerRepository = customerRepository;
+    }
+
+    public Customer saveCustomer(Customer customer){
+        customer.setId(String.valueOf(Math.random()*100));
+        return customerRepository.save(customer);
     }
 
 

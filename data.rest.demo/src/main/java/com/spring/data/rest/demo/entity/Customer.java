570a3b231aaea0c465378165978d483e065177cf
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-05-11 20:30:45 +0600

    spring data rest intregate

--- data.rest.demo/src/main/java/com/spring/data/rest/demo/entity/Customer.java
+++ data.rest.demo/src/main/java/com/spring/data/rest/demo/entity/Customer.java
@@ -1,11 +1,14 @@
 package com.spring.data.rest.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Customer {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private  String id;
     private  String name;
 

f53270a8a30bdcbc4250ff1ab94441357af66967
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-05-12 16:50:14 +0600

    swagger implemented

--- data.rest.demo/src/main/java/com/spring/data/rest/demo/Application.java
+++ data.rest.demo/src/main/java/com/spring/data/rest/demo/Application.java
@@ -3,12 +3,19 @@ package com.spring.data.rest.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
+@EnableWebMvc
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+
+
 
 }

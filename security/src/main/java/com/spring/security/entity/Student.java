785e3ca509e6c9ccac9a93d8e401d36493f1cfeb
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-05-15 14:39:02 +0600

    create StudentRepository

--- security/src/main/java/com/spring/security/entity/Student.java
+++ security/src/main/java/com/spring/security/entity/Student.java
@@ -5,10 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.UUID;
+
 @Entity
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private UUID id;
     private String name;
 }
---
+++ security/src/main/java/com/spring/security/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.spring.security.repository;
+
+import com.spring.security.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+}

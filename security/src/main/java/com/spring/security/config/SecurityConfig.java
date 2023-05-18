a902a131403e177de551b2670dbc70eb7dfdb044
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-05-18 16:43:55 +0600

    config InMemoryUserDetailsManger

--- security/src/main/java/com/spring/security/config/SecurityConfig.java
+++ security/src/main/java/com/spring/security/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.spring.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,5 +19,20 @@ public class SecurityConfig {
               .and()
               .httpBasic();;
         return http.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails zahid = User.builder().username("zahid")
+                .password("{noop}zahid123").roles("STUDENT").build();
+
+        UserDetails hasan = User.builder().username("hasan")
+                .password("{noop}hasan123").roles("TEACHER").build();
+
+
+        UserDetails sahin = User.builder().username("sahin")
+                .password("{noop}sahin123").roles("TEACHER","ADMIN").build();
+
+        return new InMemoryUserDetailsManager(zahid,hasan,sahin);
+
     }
 }
